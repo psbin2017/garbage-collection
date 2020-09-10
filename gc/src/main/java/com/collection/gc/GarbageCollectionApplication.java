930d132a882002e83d050548713f5e0aa4769c43
psbin2017 <33011631+psbin2017@users.noreply.github.com>
2020-09-10 13:02:29 +0900

    init project

---
+++ gc/src/main/java/com/collection/gc/GarbageCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.collection.gc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarbageCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarbageCollectionApplication.class, args);
+	}
+
+}
---
+++ gc/src/test/java/com/collection/gc/GarbageCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collection.gc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarbageCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
