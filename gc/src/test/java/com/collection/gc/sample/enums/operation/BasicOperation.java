1849f1dfd3b638a28521a59c3051d87945db8bce
psbin2017 <123qkrtnqls@gmail.com>
2021-03-05 20:50:43 +0900

    📝 이펙티브 자바 3E
    💎 6장: 열거 타입과 애노테이션
    ✨ 아이템 38: 확장할 수 있는 열거 타입이 필요하면 인터페이스를 사용하라

--- gc/src/test/java/com/collection/gc/sample/enums/operation/BasicOperation.java
+++ gc/src/test/java/com/collection/gc/sample/enums/operation/BasicOperation.java
@@ -1,11 +1,11 @@
-package com.collection.gc.sample.enums;
+package com.collection.gc.sample.enums.operation;
 
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public enum ConstantSpecificBody {
+public enum BasicOperation implements Operation {
     PLUS("+") {
         public double apply(double x, double y) {
             return x + y;
@@ -29,7 +29,7 @@ public enum ConstantSpecificBody {
 
     private final String symbol;
 
-    ConstantSpecificBody(String symbol) {
+    BasicOperation(String symbol) {
         this.symbol = symbol;
     }
 
@@ -38,28 +38,29 @@ public enum ConstantSpecificBody {
         return symbol;
     }
 
+    private static final Map<String, BasicOperation> stringToEnum = Stream.of(values()).collect(Collectors.toMap(Object::toString, e -> e));
+
+    public static Optional<BasicOperation> fromString(String symbol) {
+        return Optional.ofNullable( stringToEnum.get(symbol) );
+    }
+
+
     /**
      * 동작은 하지만 깨지기 쉬운 코드.
      * 상수가 추가되면 반드시 해당 case 문도 추가되어야 한다.
      */
     /**
-    public double badApply(double x, double y) {
-        switch (this) {
-            case PLUS: return x + y;
-            case MINUS: return x - y;
-            case TIMES: return x * y;
-            case DIVIDE: return x / y;
-        }
-        throw new AssertionError("선언되지 않은 연산");
-    }
-    */
+     public double badApply(double x, double y) {
+     switch (this) {
+     case PLUS: return x + y;
+     case MINUS: return x - y;
+     case TIMES: return x * y;
+     case DIVIDE: return x / y;
+     }
+     throw new AssertionError("선언되지 않은 연산");
+     }
+     */
 
     // 새로운 상수가 추가되어도 추상 메소드의 구현을 잊을 수 없게 된다.
-    public abstract double apply(double x, double y);
-
-    private static final Map<String, ConstantSpecificBody> stringToEnum = Stream.of(values()).collect(Collectors.toMap(Object::toString, e -> e));
-
-    public static Optional<ConstantSpecificBody> fromString(String symbol) {
-        return Optional.ofNullable( stringToEnum.get(symbol) );
-    }
+    // public abstract double apply(double x, double y);
 }
---
+++ gc/src/test/java/com/collection/gc/sample/enums/operation/Operation.java
@@ -0,0 +1,5 @@
+package com.collection.gc.sample.enums.operation;
+
+public interface Operation {
+    public double apply(double x, double y);
+}
