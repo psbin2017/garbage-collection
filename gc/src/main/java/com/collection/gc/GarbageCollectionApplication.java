bfddb96ac5b13fc7a92cf7e2e699d963b3599353
psbin2017 <123qkrtnqls@gmail.com>
2021-04-04 16:49:42 +0900

    ✏ scheduling-tasks 예제
    https://spring.io/guides/gs/scheduling-tasks/

--- gc/src/main/java/com/collection/gc/GarbageCollectionApplication.java
+++ gc/src/main/java/com/collection/gc/GarbageCollectionApplication.java
@@ -3,8 +3,14 @@ package com.collection.gc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * `@EnableAspectJAutoProxy` {@link com.collection.gc.aspect.LoggingAspect}
+ * `@EnableScheduling` {@link com.collection.gc.schedule.ScheduleTasks}}
+ */
 @EnableAspectJAutoProxy
+@EnableScheduling
 @SpringBootApplication
 public class GarbageCollectionApplication {
 
